5f27b0e933a03e58d2ce7bc380db9d8f6147e60f
Maksym <maxpavliy@gmail.com>
2025-02-12 19:27:10 +0100

    sign up page + footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 
+//Shop components:
 import { HomeComponent } from './components/home/home.component';
 import { CategoryPageComponent } from './components/category-page/category-page.component';
 import { ProductPageComponent } from './components/product-page/product-page.component';
+
+//Auth components:
+import { RegistrationComponent } from './auth/components/registration/registration.component';
+import { LoginComponent } from './auth/components/login/login.component';
 
 import { PanelComponent } from './admin/components/panel/panel.component';
 
@@ -12,6 +17,9 @@ export const routes: Routes = [
 
     { path: "products/:category", component: CategoryPageComponent},
     { path: "products/:category/:id", component: ProductPageComponent},
+
+    { path: "auth/registration", component: RegistrationComponent},
+    { path: "auth/login", component: LoginComponent},
 
     { path: "admin", component: PanelComponent}
 ];
---
+++ src/app/auth/models/register.model.ts
@@ -0,0 +1,6 @@
+export interface registerModel {
+    email: string,
+    password: string,
+    confirmPwd: string,
+    country: string
+}
