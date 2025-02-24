47ca38c0412a6572eda24f89e4a4beb63bef7e52
Maksym <maxpavliy@gmail.com>
2025-02-24 22:48:46 +0100

    reset

--- src/app/services/product-services/product.service.ts
+++ src/app/services/product-services/product.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { apiUrl } from '../../../env/product_api.environment';
+import { environment } from '../../../environments/environment';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  private apiUrl = environment.productUrl;
+
   constructor(private http: HttpClient) {};
 
   getProduct(id: string, category: string): Observable<any> {
-    return this.http.get<any>(apiUrl + category + "/" + id);
+    return this.http.get<any>(`${this.apiUrl}${category}/${id}`);
   }
 }
