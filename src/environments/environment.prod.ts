da879bd4ae0b6662a04737a32c01d872d9a63eaf
Maksym <maxpavliy@gmail.com>
2025-02-24 22:59:31 +0100

    env file fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    authUrl: process.env['NG_ENV_AUTH_URL'] || "https://macha-server.vercel.app/auth/",
-    productUrl: process.env['NG_ENV_PRODUCT_URL'] || "https://macha-server.vercel.app/products/"
-  };
+    authUrl: "VERCEL_ENV_AUTH_URL",
+    productUrl: "VERCEL_ENV_PRODUCT_URL"
+};
